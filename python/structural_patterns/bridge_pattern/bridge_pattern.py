4c3bfb38bd25f16e91f8782f53961c33d0f24636
施意波 <2322540522@qq.com>
2023-05-08 16:18:16 +0800

    组合模式完成

--- python/structural_patterns/bridge_pattern/bridge_pattern.py
+++ python/structural_patterns/bridge_pattern/bridge_pattern.py
@@ -1,38 +1,76 @@
 """
-桥接模式
-直接上定义：将抽象部分与它的实现部分分离，使它们都可以独立地变化。
-这个定义很明显比较模糊，我们这边采用示例的方式进行解释
+组合模式
+定义：组合，将对象组合成树状结构，来表示业务逻辑上的[部分-整体]层次，这种组合使单个对象和组合对象的使用方法一样。
+简单的说就是，当你需要设置一个树形结构的类的情况的情况下可以使用，前节点包含后节点的类就是组合模式
 
 使用场景：
-假设有三种品牌的汽车，并且分别有3种动力-汽、电、混合，如果要将所有的情况都表示出来，那就有9种情况，需要写9个子类，
-并且随着品牌的增加，子类也会成百上千的增长，这时候就考虑使用桥接模式
+进行前后端数据传递的时候感觉可以使用，后续可以尝试看看使用
 """
 
 
-class Car:
-    def __init__(self, engine):
-        self.engine = engine
-        self.brand = ""
+class Base:
+    def __init__(self):
+        self._parent = None
 
-    def run(self):
-        print(f"牌子为：{self.brand}，引擎为：{self.engine}")
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        self._parent = value
+
+    def add(self, comp):
+        pass
+
+    def remove(self, comp):
+        pass
+
+    def display(self):
+        pass
 
 
-class CarA(Car):
-    def __init__(self, engine):
-        super().__init__(engine)
-        self.brand = "A"
+# 叶子节点
+class Leaf(Base):
+    def __init__(self,name):
+        super().__init__()
+        self.name = name
 
-class CarB(Car):
-    def __init__(self, engine):
-        super().__init__(engine)
-        self.brand = "B"
+    def display(self):
+        strtemp = f'本叶子节点为：{self.name}'
+        print(strtemp)
 
-if __name__ == '__main__':
-    """
-    通过将引擎的选择权交给了用户，可以有效减少子类的创建，只需要在使用的时候使用对应的引擎即可，在python中有个框架使用了mixin的方式，就是使用了这个思想
-    """
-    CarA("电动").run()
-    CarB("燃油").run()
+
+# 枝节点
+class Composite(Base):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.children = []
+
+    def add(self, comp):
+        self.children.append(comp)
+
+    def remove(self, comp):
+        self.children.remove(comp)
+
+    def display(self):
+        strtemp = f'这是根节点为：{self.name}'
+        print(strtemp)
+        for comp in self.children:
+            comp.display()
+
+if __name__ == "__main__":
+    # 生成树根
+    root = Composite("root")
+    # 根上长出2个叶子
+    root.add(Leaf('leaf A'))
+    root.add(Leaf('leaf B'))
+    composite = Composite("composite A")
+
+    root.add(composite)
+
+    composite.add(Leaf('leaf C'))
+    root.display()
 
 
